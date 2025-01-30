c43c6fc892950f803cf56ea2c61d14e1f9f90cba
Hema K <hema@qubernet.com>
2025-01-30 12:18:32 +0530

    initial commit

---
+++ mui-store/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className="text-3xl font-bold underline">Learn React</h3>    
+    </div>
+  );
+}
+
+export default App;
